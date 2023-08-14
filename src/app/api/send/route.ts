5fc6c01d4f002cc920c6c0ad507ead2e32953a2f
SameerJadav <sameerjadav010@gmail.com>
2023-08-14 19:21:15 +0530

    basic mail template

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -8,10 +8,15 @@ const resend = new Resend(env.RESEND_API_KEY)
 export async function POST() {
   try {
     const data = await resend.emails.send({
-      from: "niharparmar9978@sameerjadav.me",
-      to: "sameerjadav010@gmail.com",
-      subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }),
+      from: "fees@magicbeats.in",
+      to: "premmakwana0718@gmail.com",
+      subject: "Payment Confirmation - Music Academy Fees",
+      react: EmailTemplate({
+        studentName: "Prem Makwana",
+        amountPaid: 1500,
+        dateOfPayment: "11/08/2023",
+        paymentMethod: "Cash",
+      }),
     })
 
     return NextResponse.json(data)
--- src/components/EmailTemplate.tsx
+++ src/components/EmailTemplate.tsx
@@ -1,13 +1,36 @@
 import * as React from "react"
 
 interface EmailTemplateProps {
-  firstName: string
+  studentName: string
+  amountPaid: number
+  dateOfPayment: string
+  paymentMethod: string
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
-  firstName,
+  studentName,
+  amountPaid,
+  dateOfPayment,
+  paymentMethod,
 }) => (
   <div>
-    <h1>Welcome, {firstName}!</h1>
+    <p>Dear {studentName},</p>
+    <p>
+      We are writing to confirm that we have received your payment for the fees
+      at Magic Beats Music Academy. Thank you for your prompt payment.
+    </p>
+    <p>Here are the details of your payment:</p>
+    <ul>
+      <li>Name: {studentName}</li>
+      <li>Amount Paid: {amountPaid}</li>
+      <li>Date of Payment: {dateOfPayment}</li>
+      <li>Payment Method: {paymentMethod}</li>
+    </ul>
+    <p>
+      Please keep this email as your receipt for the payment. If you have any
+      questions or concerns, please do not hesitate to contact us.
+    </p>
+    <p>Best regards,</p>
+    <p>Magic Beats</p>
   </div>
 )
