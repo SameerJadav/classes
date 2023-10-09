d516cb3bc9e5c08eda4db2916915b2e8ef4e2cc1
SameerJadav <sameerjadav010@gmail.com>
2023-10-10 00:42:53 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Magic Beats",
+  description: "Magic Beats Music Academy",
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
