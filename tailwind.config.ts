8f4fde35071e21af5d24c6f2d3d28c15f0693ec4
SameerJadav <sameerjadav010@gmail.com>
2023-10-10 19:42:25 +0530

    loading button styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
-import { slateDark } from "@radix-ui/colors";
+import { slateDark, yellowDark } from "@radix-ui/colors";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/components/**/*.{ts,tsx}", "./src/app/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      colors: { ...slateDark },
+      colors: { ...slateDark, ...yellowDark },
     },
   },
   future: {
