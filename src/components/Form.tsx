3808d8f94f1525d722c55143bdcdf2945acf2611
SameerJadav <sameerjadav010@gmail.com>
2023-10-10 19:24:42 +0530

    refactore form code

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,45 +5,56 @@ import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
 export default function Form() {
-  const [emailAddress, setEmailAddress] = useState("");
-  const [studentName, setStudentName] = useState("");
-  const [amountPaid, setAmountPaid] = useState("");
-  const [dateOfPayment, setDateOfPayment] = useState("");
-  const [paymentMethod, setPaymentMethod] = useState("");
-
-  const { mutate, status } = useMutation({
-    mutationFn: async () => {
-      return await axios.post("/api/send", {
-        emailAddress,
-        studentName,
-        amountPaid,
-        dateOfPayment,
-        paymentMethod,
-      });
-    },
+  const [formData, setFormData] = useState({
+    emailAddress: "",
+    studentName: "",
+    amountPaid: "",
+    dateOfPayment: "",
+    paymentMethod: "",
   });
+
+  const allFieldsFilled = Object.values(formData).every(
+    (field) => field !== "",
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    if (
-      emailAddress !== "" &&
-      studentName !== "" &&
-      amountPaid !== "" &&
-      dateOfPayment !== "" &&
-      paymentMethod !== ""
-    )
-      mutate();
+    if (allFieldsFilled) {
+      mutate(formData);
+    }
   };
+
+  const { mutate, status } = useMutation({
+    mutationFn: async (data: typeof formData) =>
+      await axios.post("/api/send", data),
+    onSuccess: () => {
+      setFormData({
+        emailAddress: "",
+        studentName: "",
+        amountPaid: "",
+        dateOfPayment: "",
+        paymentMethod: "",
+      });
+    },
+  });
 
   return (
     <form className="mt-4 grid grid-cols-2 gap-4">
       <input
         type="text"
-        name="to"
+        name="emailAddress"
         id="to"
         required
-        value={emailAddress}
-        onChange={(e) => setEmailAddress(e.target.value)}
+        value={formData.emailAddress}
+        onChange={handleChange}
         disabled={status === "loading"}
         placeholder="Email Address"
         className="col-span-2 rounded-md border border-slate7 bg-slate3 px-3 py-1.5 transition-colors duration-200 ease-in placeholder:text-slate11 hover:border-slate8 focus:border-slate9 focus:outline-none"
@@ -53,8 +64,8 @@ export default function Form() {
         name="studentName"
         id="studentName"
         required
-        value={studentName}
-        onChange={(e) => setStudentName(e.target.value)}
+        value={formData.studentName}
+        onChange={handleChange}
         disabled={status === "loading"}
         placeholder="Student Name"
         className="rounded-md border border-slate7 bg-slate3 px-3 py-1.5 transition-colors duration-200 ease-in placeholder:text-slate11 hover:border-slate8 focus:border-slate9 focus:outline-none"
@@ -64,19 +75,19 @@ export default function Form() {
         name="amountPaid"
         id="amountPaid"
         required
-        value={amountPaid}
-        onChange={(e) => setAmountPaid(e.target.value)}
+        value={formData.amountPaid}
+        onChange={handleChange}
         disabled={status === "loading"}
         placeholder="Amount Paid"
         className="rounded-md border border-slate7 bg-slate3 px-3 py-1.5 transition-colors duration-200 ease-in placeholder:text-slate11 hover:border-slate8 focus:border-slate9 focus:outline-none"
       />
       <input
         type="text"
-        name="date"
+        name="dateOfPayment"
         id="date"
         required
-        value={dateOfPayment}
-        onChange={(e) => setDateOfPayment(e.target.value)}
+        value={formData.dateOfPayment}
+        onChange={handleChange}
         disabled={status === "loading"}
         placeholder="Date of Payment"
         className="rounded-md border border-slate7 bg-slate3 px-3 py-1.5 transition-colors duration-200 ease-in placeholder:text-slate11 hover:border-slate8 focus:border-slate9 focus:outline-none"
@@ -86,15 +97,16 @@ export default function Form() {
         name="paymentMethod"
         id="paymentMethod"
         required
-        value={paymentMethod}
-        onChange={(e) => setPaymentMethod(e.target.value)}
+        value={formData.paymentMethod}
+        onChange={handleChange}
         disabled={status === "loading"}
         placeholder="Payment Method"
         className="rounded-md border border-slate7 bg-slate3 px-3 py-1.5 transition-colors duration-200 ease-in placeholder:text-slate11 hover:border-slate8 focus:border-slate9 focus:outline-none"
       />
       <button
         onClick={handleClick}
-        className="col-span-2 w-full rounded-md border border-slate7 bg-slate12 py-1.5 font-medium text-slate1 transition-colors duration-200 ease-in hover:border-slate8 hover:bg-slate11"
+        disabled={!allFieldsFilled}
+        className="col-span-2 w-full rounded-md bg-slate12 py-1.5 font-medium text-slate1 transition-colors duration-200 ease-in hover:bg-slate11 disabled:cursor-not-allowed disabled:bg-slate5 disabled:text-slate11"
       >
         Send
       </button>
